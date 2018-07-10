abc3b58159cfccf42c0506a187f7902954f93fa9
Clement Meyer <meyclem@gmail.com>
2018-07-10 10:06:36 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        vue-osc-test
+      </h1>
+      <h2 class="subtitle">
+        My fabulous Nuxt.js project
+      </h2>
+      <div class="links">
+
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+}
+</script>
+
+<style>
+</style>
