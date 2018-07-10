f56f718071b24870221d2422c232064a67aa40c4
Clement Meyer <meyclem@gmail.com>
2018-07-10 19:32:24 +0200

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,50 @@
 <template>
-  <section class="container">
-    <div>
-      <h1 class="title">
-        vue-osc-test
-      </h1>
-      <h2 class="subtitle">
-        My fabulous Nuxt.js project
-      </h2>
-      <div class="links">
-
+  <div class="relative w-full h-screen">
+        <div class="pad absolute pin" @touchmove="movePoint()">
+          <p class="text-green">x: {{ clientX }}</p>
+          <p class="text-green">y: {{ clientY }}</p>
+          <div class="point"
+          :style="'top: ' + (clientY - 10) + 'px; left: ' + (clientX - 10) + 'px;'">
+          </div>
+        </div>
       </div>
-    </div>
-  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-}
+  export default {
+    data() {
+      return {
+        clientX: 0,
+        clientY: 0,
+        text: ''
+      }
+    },
+    methods: {
+      movePoint() {
+        this.clientX = Math.floor(event.touches[0].clientX);
+        this.clientY = Math.floor(event.touches[0].clientY);
+        this.sendMouseCoords()
+      },
+      sendMouseCoords() {
+        console.log(`x: ${this.clientX} || y: ${this.clientY}`)
+      }
+    },
+    mounted() {
+    }
+  }
 </script>
 
-<style>
+<style scoped>
+.pad {
+  background-color: black;
+  overflow: hidden;
+  touch-action: none;
+}
+.point {
+  position: absolute;
+  border-radius: 50%;
+  height: 20px;
+  width: 20px;
+  background-color: white;
+}
 </style>
