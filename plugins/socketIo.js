bfba7d40e7b7fa6a9a6747505fce6ff26713d948
Clement Meyer <meyclem@gmail.com>
2018-07-11 11:35:50 +0200

    changes ws_url again

--- plugins/socketIo.js
+++ plugins/socketIo.js
@@ -1,4 +1,8 @@
 import io from 'socket.io-client'
-const socket = io('http://127.0.0.1:26015')
+
+const host = process.env.HOST || '127.0.0.1'
+const port = process.env.PORT || 3000
+
+const socket = io('http://' + host + ':' + port)
 
 export default socket
