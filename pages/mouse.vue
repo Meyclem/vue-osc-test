3ce9dc650a05545d55f908514e36b38bd45665c5
Clement Meyer <meyclem@gmail.com>
2018-07-11 11:44:43 +0200

    tests ws_url

---
+++ pages/mouse.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="relative w-full h-screen">
+        <div class="pad absolute pin" @touchmove="movePoint()">
+          <p class="text-green">x: {{ clientX }}</p>
+          <p class="text-green">y: {{ clientY }}</p>
+          <div class="point"
+          :style="'top: ' + (clientY - 10) + 'px; left: ' + (clientX - 10) + 'px;'">
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+import socket from '~/plugins/socketIo.js'
+
+export default {
+  data() {
+    return {
+      clientX: 0,
+      clientY: 0,
+      text: ''
+    }
+  },
+  methods: {
+    movePoint() {
+      this.clientX = Math.floor(event.touches[0].clientX);
+      this.clientY = Math.floor(event.touches[0].clientY);
+      socket.emit('mouse', this.sendMouseCoords())
+    },
+    sendMouseCoords() {
+      const text = `x: ${this.clientX} || y: ${this.clientY}`
+      console.log(text)
+      return text
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style scoped>
+.pad {
+  background-color: black;
+  overflow: hidden;
+  touch-action: none;
+}
+.point {
+  position: absolute;
+  border-radius: 50%;
+  height: 20px;
+  width: 20px;
+  background-color: white;
+}
+</style>
--- plugins/socketIo.js
+++ plugins/socketIo.js
@@ -3,6 +3,10 @@ import io from 'socket.io-client'
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
-const socket = io('http://' + host + ':' + port)
+const wsUrl = 'http://' + host + ':' + port
+console.log(wsUrl)
+console.log('bite')
+
+const socket = io(wsUrl)
 
 export default socket
